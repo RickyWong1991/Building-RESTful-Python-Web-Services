cf734ee52fd8c26e681aefce28fe6d1c63d641cd
Unknown <sskysstar@gmail.com>
2018-06-01 16:13:56 +0800

    Revert "books source code"
    This reverts commit 404ef9567a42030fbf64a893cbb28a2a7664ec72.

--- Chapter 1/restful_python_chapter_01_01/gamesapi/games/tests.py
+++
@@ -1,9 +0,0 @@
-"""
-Book: Building RESTful Python Web Services
-Chapter 1: Developing RESTful APIs with Django
-Author: Gaston C. Hillar - Twitter.com/gastonhillar
-Publisher: Packt Publishing Ltd. - http://www.packtpub.com
-"""
-from django.test import TestCase
-
-# Create your tests here.
--- Chapter 3/restful_python_chapter_03_01/gamesapi/games/tests.py
+++
@@ -1,9 +0,0 @@
-"""
-Book: Building RESTful Python Web Services
-Chapter 3: Improving and adding authentication to an API with Django
-Author: Gaston C. Hillar - Twitter.com/gastonhillar
-Publisher: Packt Publishing Ltd. - http://www.packtpub.com
-"""
-from django.test import TestCase
-
-# Create your tests here.
